ab0ac29d634e11fe1e060a07a63a58edc33cbf54
Austin <aturn9880@gmail.com>
2021-09-30 23:26:48 -0400

    commit tree b86478907c2546459ea377940020f23d932a4370

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
 
+    def initialize(book_title)
+        @title = book_title
+    end
 
+    # def author= (author_name)
+    #     @author = author_name
+    # end
+    
+    # def page_count= (count)
+    #     @page_count = count
+    # end
+    
+    # def genre= (book_genre)
+    #     @genre = book_genre
+    # end
+
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
